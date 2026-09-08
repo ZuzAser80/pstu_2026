b385d4b6ffd3aa2d4c3d2f385b5954870f842230
= <dde4th@gmail.com>
2026-09-08 07:28:30 +0500

    init

---
+++ oop/labs/Lab1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {        
+        double x, m, n;
+        Console.WriteLine("input m & n, with decimal (e.g. 1.0):");
+        if (double.TryParse(Console.ReadLine(), out m) && double.TryParse(Console.ReadLine(), out n))
+        {
+            Console.WriteLine($"1: {m---n}");
+            Console.WriteLine($"2: {(m++)<n}");
+            Console.WriteLine($"3: {(n++)>m}");
+        } else
+        {
+            Console.WriteLine("Input data incorrect, (m or n), not numerical?");
+        }
+        Console.WriteLine("input x with decimal (e.g. 1.0):: ");
+        if(double.TryParse(Console.ReadLine(), out x) || x < -1 || x > 1)
+        {
+            Console.WriteLine($"4 : {Math.Pow(x, 4) - Math.Cos(Math.Asin(x))}");
+        } else
+        {
+            Console.WriteLine("Input data incorrect, x is not numerical or is out of bounds (-1 <= x <= 1).");
+        }
+
+    }
+
+    static bool Task2(double x1, double y1)
+    {        
+        return Math.Sqrt(x1 * x1 + y1 * y1) <= 1;
+    }
+}
---
+++ oop/labs/Lab2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n, k, sum;
+        sum = 0;
+        Console.WriteLine("input n: ");
+        if (int.TryParse(Console.ReadLine(), out n))
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (int.TryParse(Console.ReadLine(), out k))
+                {
+                    sum += k % 2 == 0 ? 0 : k;
+                } else
+                {
+                    Console.WriteLine("incorrect k input, couldn't parse");
+                    return;
+                }
+            }
+        } else
+        {
+            Console.WriteLine("incorrect n input, couldn't parse");
+        }
+    }
+
+}
