f016c57ee3b6b3be20af25f4043f99223c138706
ZuzAser80 <dde4th@gmail.com>
2026-09-13 16:26:54 +0500

    lab 5-6 start

--- oop/labs/Lab5/Program.cs
+++ oop/labs/Lab5/Program.cs
@@ -1,1 +1,136 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static readonly Random _random = new();
+    const int MinValue = -100, MaxValue = 100;
+
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("1. Двумерные массивы \n 2. Рваные массивы");
+        int choice = ReadInput("?: ");
+        switch (choice)
+        {
+            case 1:
+                System.Console.WriteLine("1. Ввести вручную \n 2. Рандом \n 3. Выход");
+                choice = ReadInput("?: ");
+                switch (choice)
+                {
+                    case 1:
+                        InputRectangularArray();
+                        break;
+                    case 2:
+                        PrintArray(RandomRectangularArray());
+                        break;
+                }
+                break;
+
+            case 2:
+                System.Console.WriteLine("1. Ввести вручную \n 2. Рандом \n 3. Выход");
+                choice = ReadInput("?: ");
+                switch (choice)
+                {
+                    case 1:
+                        InputJaggedArray();
+                        break;
+                    case 2:
+                        PrintArray(RandomJaggedArray());
+                        break;
+                }
+                break;
+        }
+
+
+
+    }
+
+    static int[][] InputJaggedArray()
+    {
+        int n = ReadInput("n: ");
+        var result = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            int m = ReadInput("m: ");
+            result[i] = new int[m];
+            for (int j = 0; j < m; j++)
+                result[i][j] = ReadInput($"a[{i},{j}]: ");
+        }
+        return result;
+    }
+
+    static int[][] RandomJaggedArray()
+    {
+        int n = ReadInput("n: ");
+        int minLen = ReadInput("min length: ");
+        int maxLen = ReadInput("max length: ");
+        var result = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            int m = _random.Next(minLen, maxLen + 1);
+            result[i] = new int[m];
+            for (int j = 0; j < m; j++)
+                result[i][j] = _random.Next(MinValue, MaxValue + 1);
+        }
+        return result;
+    }
+
+    static int[,] RandomRectangularArray()
+    {
+        int n = ReadInput("n: ");
+        int m = ReadInput("m: ");
+        var result = new int[n, m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                result[i, j] = _random.Next(MinValue, MaxValue + 1);
+        return result;
+    }
+
+    static void PrintArray(int[,] arr)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++) {
+                System.Console.Write(arr[i, j] + " ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+
+    static void PrintArray(int[][] arr)
+    {
+        foreach (var row in arr)
+        {
+            foreach (int value in row)
+                System.Console.Write(value + " ");
+            System.Console.WriteLine();
+        }
+    }
+
+    static int[,] InputRectangularArray()
+    {
+        int n = ReadInput("n: ");
+        int m = ReadInput("m: ");
+        var result = new int[n, m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                result[i, j] = ReadInput($"a[{i},{j}]: ");
+        return result;
+    }
+
+    static int ReadInput(string prompt)
+    {
+        int result;
+        bool isParsed;
+        do
+        {
+            System.Console.WriteLine(prompt);
+            isParsed = int.TryParse(System.Console.ReadLine(), out result);
+            if (!isParsed)
+                System.Console.WriteLine("Invalid number. Please enter an integer.");
+        } while (!isParsed);
+        return result;
+    }
+
+
+}
+
--- oop/labs/Lab6/Program.cs
+++ oop/labs/Lab6/Program.cs
@@ -1,1 +1,60 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int choice;
+        bool validChoice = false;
+        do
+        {
+            System.Console.WriteLine("Choose input:");
+            System.Console.WriteLine("1. Enter your own string");
+            System.Console.WriteLine("2. Use a test string");
+            validChoice = int.TryParse(Console.ReadLine(), out choice) && (choice == 1 || choice == 2);
+            if (!validChoice)
+            {
+                System.Console.WriteLine("invalid choice");
+            }
+        } while (!validChoice);
+
+        string? input = null;
+        bool isNotNull = false;
+        bool moreThenOneSentence = false;
+        do
+        {
+            if (choice == 2 && input == null)
+            {
+                input = "В лесу родилась елочка. В лесу она росла. Зимой и летом стройная, зеленая была.";
+            }
+            else
+            {
+                System.Console.WriteLine("Input sentence: ");
+                input = Console.ReadLine();
+            }
+            isNotNull = input != null;
+            if (!isNotNull || input == "")
+            {
+                System.Console.WriteLine("string is null");
+                continue;
+            }
+            
+            moreThenOneSentence = Regex.IsMatch(input, @"^(?:[^.!?]*[.!?](?=[\p{L}\s]|$)){2}");
+            if (!moreThenOneSentence)
+            {
+                System.Console.WriteLine("string contains only one sentence:");                
+                continue;
+            }
+            var sentences = Regex.Split(input, @"(?<=[.!?])\s+");
+            string result = "";
+            result += sentences?[sentences.Count() - 1];
+            for (int i = 1; i <= sentences?.Count() - 2; i++)
+            {
+                result += sentences?[i];
+            }
+            result += sentences?[0];    
+            System.Console.WriteLine($"result: {result}");
+        } while (!isNotNull || !moreThenOneSentence);      
+    }
+}
